62f531304218a6c7a63d0e12ec4d335ca715f2c1
Leon <jiangxilee@gmail.com>
2017-09-08 15:28:08 +0800

    add-role-for-userapi

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -26,6 +26,7 @@ class UserController extends MainController
                 'id',
                 'username',
                 'phone',
+                'role',
                 'sex',
                 'country',
                 'province',
