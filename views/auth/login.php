8fe11722b7ffbc9c34cf15b577c66d5c22728dbd
cptbtptpe <1159882790@qq.com>
2018-01-29 17:33:54 +0800

    登录

--- views/auth/login.php
+++ views/auth/login.php
@@ -5,6 +5,7 @@ $params = \Yii::$app->params;
 \Yii::$app->params['ng_ctrl'] = 'auth';
 ?>
 
+<marquee class="news">如果在微信端有过数据，建议从公众号菜单进入并完成首次绑定手机号，此后方可在普通浏览器中自由登录，以保证您的数据不会丢失。</marquee>
 <div class="login-bg" ng-init="info.extra = '<?= $extra ?>'">
     <div class="input-group">
         <input class="form-control phone input-lg" ng-model="info.phone" placeholder="手机号码">
